3d67b5ebebbb77f993d2b175f959cc8a591615b4
ahmednule <nuleahmed6@gmail.com>
2025-11-29 12:29:29 +0300

    fix: removed firebase initialization code directly inside environment.ts and environment.prod.ts left them as excecuatble data only such that I wont get  errors during run time

--- apps/kujali/src/app/environments/environment.prod.ts
+++ apps/kujali/src/app/environments/environment.prod.ts
@@ -1,16 +1,13 @@
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyBl_SdgPAQxymGzoI9MPvBa__HdP4nTs4Q",
-  authDomain: "kujali-36a76.firebaseapp.com",
-  projectId: "kujali-36a76",
-  storageBucket: "kujali-36a76.firebasestorage.app",
-  messagingSenderId: "682904600196",
-  appId: "1:682904600196:web:012cd7b27aed072b950cd1",
-  measurementId: "G-WYNTR0M79F"
+export const environment = {
+  production: true,
+  useEmulators: false,
+  firebase: {
+    apiKey: "AIzaSyBl_SdgPAQxymGzoI9MPvBa__HdP4nTs4Q",
+    authDomain: "kujali-36a76.firebaseapp.com",
+    projectId: "kujali-36a76",
+    storageBucket: "kujali-36a76.firebasestorage.app",
+    messagingSenderId: "682904600196",
+    appId: "1:682904600196:web:012cd7b27aed072b950cd1",
+    measurementId: "G-WYNTR0M79F"
+  }
 };
-
-// Firebase initialization
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
